e4cacd8fef3f9216e7bcb42d85b4fbd25ab1b9df
Keeno Lee <keenoleecw@gmail.com>
2023-08-05 23:52:11 -0400

    update index and footer

---
+++ components/index.ts
@@ -0,0 +1,8 @@
+import Footer from "./Footer";
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import SeachBar from "./SeachBar";
+import CommonButton from "./common-ui/CommonButton";
+import CommonFilter from "./common-ui/CommonFilter";
+
+export { Hero, CommonButton, Navbar, Footer, CommonFilter, SeachBar };
